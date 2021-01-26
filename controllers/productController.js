51baa29da38ce5435bdc811f4504eb58c70180ad
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-01-26 21:41:50 +0200

    Add search

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ const { validateProduct } = require('./helpers/productHelpers');
 const router = Router();
 
 router.get('/', (req, res) => {
-    let products = productService.getAll();
+    let products = productService.getAll(req.query);
 
     res.render('home', {title: 'Browse', products });
 });
@@ -15,14 +15,17 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', validateProduct, (req, res) => {
-    productService.create(req.body, (err) => {
-        if (err) {
-            return res.status(500).end();
-        }
+    // productService.create(req.body, (err) => {
+    //     if (err) {
+    //         return res.status(500).end();
+    //     }
 
-        res.redirect('/products');
-    });
+    //     res.redirect('/products');
+    // });
 
+    productService.create(req.body)
+        .then(() => res.redirect('/products'))
+        .catch(() => res.status(500).end())
 });
 
 router.get('/details/:productId', (req, res) => {
--- services/productService.js
+++ services/productService.js
@@ -1,12 +1,26 @@
 const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 let productsData = require('../config/products.json');
 
-function getAll() {
-    return productsData;
+function getAll(query) {
+    let result = productsData;
+    
+    if (query.search) {
+        result = result.filter(x => x.name.toLowerCase().includes(query.search));
+    }
+    
+    if (query.from) {
+        result = result.filter(x => Number(x.level) >= query.from);
+    }
+    
+    if (query.to) {
+        result = result.filter(x => Number(x.level) <= query.to);
+    }
+
+    return result;
 }
 
 function getOne(id) {
@@ -24,11 +38,16 @@ function create(data, callback) {
 
     productsData.push(cube);
 
-    fs.writeFile(
-        path.join(__dirname, '../config/products.json'), 
-        JSON.stringify(productsData), 
-        callback
-    );
+    // fs.writeFile(
+    //     path.join(__dirname, '../config/products.json'), 
+    //     JSON.stringify(productsData), 
+    //     callback
+    // );
+
+    return fs.writeFile(
+        path.join(__dirname, '../config/products.json'),
+        JSON.stringify(productsData),
+    )
 }
 
 module.exports = {
