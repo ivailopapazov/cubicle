e502291de8adfc1ccb11a21a4020a1908ef712da
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-01-26 18:53:46 +0200

    Add routes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
 const config = require('./config/config');
+const routes = require('./routes');
 const app = express();
 
-// const expressConfig = require('./config/express');
-// expressConfig(app);
 require('./config/express')(app);
 
-app.get('/', (req, res) => {
-    res.render('home', {layout: false});
-});
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
---
+++ routes.js
@@ -0,0 +1,10 @@
+
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('home', {layout: false});
+});
+
+module.exports = router;
