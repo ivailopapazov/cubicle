a7e29c0117e0477f22fcde6051eebfc44058433a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-01-26 19:32:08 +0200

    Addd not found page

--- routes.js
+++ routes.js
@@ -7,5 +7,8 @@ const router = Router();
 
 router.use('/about', aboutController);
 router.use('/', productController);
+router.get('*', (req, res) => {
+    res.render('404', {layout: false});
+});
 
 module.exports = router;
