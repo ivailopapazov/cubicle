d3176aca8519d2f17ea86a7fd153fade6e905960
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-01-26 18:46:34 +0200

    Extract express config

---
+++ config/express.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+function setupExpress(app) {
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
+    
+    app.set('view engine', 'hbs');
+    
+    app.use(express.static('public'))
+}
+
+module.exports = setupExpress;
--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 
 const config = require('./config/config');
-const handlebars = require('express-handlebars');
 const app = express();
 
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}));
-
-app.set('view engine', 'hbs');
-
-app.use(express.static('public'))
+// const expressConfig = require('./config/express');
+// expressConfig(app);
+require('./config/express')(app);
 
 app.get('/', (req, res) => {
     res.render('home', {layout: false});
